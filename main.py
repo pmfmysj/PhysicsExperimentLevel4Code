358c5cf5d6674b7ff57d40c2aed73f41da310c34
pmfmysj <pmfmysj@gmail.com>
2024-12-16 20:30:17 +0800

    加了个main

---
+++ main.py
@@ -0,0 +1,5 @@
+from pic2Numpy import pic2Numpy
+from FittingLevenbergMarquardt import lmFitting
+
+pic2Numpy()
+lmFitting()
--- pic2Numpy.py
+++ pic2Numpy.py
@@ -11,31 +11,32 @@ step = 20
 matrixs = []
 avgs = []
 
-# 循环
-for i in range(start, stop + 1, step):
+def pic2Numpy():
+    # 循环
+    for i in range(start, stop + 1, step):
 
-    imageName = f"A_{i:03d}.bmp"                        # 带0
-    image = Image.open(imageName).convert('L')          # 转为灰度图像
+        imageName = f"A_{i:03d}.bmp"                        # 带0
+        image = Image.open(imageName).convert('L')          # 转为灰度图像
 
-    iMatrix = np.array(image).ravel()                   # 转换为一维数组
-    matrixs.append(iMatrix)
-    avg = np.average(iMatrix)                           # 取平均
-    avgs.append(avg)
-    print(avg)                                          # 打印平均值    
+        iMatrix = np.array(image).ravel()                   # 转换为一维数组
+        matrixs.append(iMatrix)
+        avg = np.average(iMatrix)                           # 取平均
+        avgs.append(avg)
+        print(avg)                                          # 打印平均值    
+
+        #⬇️导出成npy
+        np.save(f"iMatrix_A_{i:03d}.npy", iMatrix)
+        print(f"A_{i:03d}'s iMatrix has saved as iMatrix_A_{i:03d}.npy")
+
+        #⬇️导出成csv
+        csvFilePath = f"A_{i:03d}.csv"                                     
+        np.savetxt(csvFilePath, iMatrix, delimiter=",", fmt='%d')          
+        print(f"A_{i:03d}'s pixelMatrix has exported: {csvFilePath}")
 
     #⬇️导出成npy
-    np.save(f"iMatrix_A_{i:03d}.npy", iMatrix)
-    print(f"A_{i:03d}'s iMatrix has saved as iMatrix_A_{i:03d}.npy")
+    np.save(f"iData.npy", avgs)
 
-    #⬇️导出成csv
-    csvFilePath = f"A_{i:03d}.csv"                                     
-    np.savetxt(csvFilePath, iMatrix, delimiter=",", fmt='%d')          
-    print(f"A_{i:03d}'s pixelMatrix has exported: {csvFilePath}")
-
-#⬇️导出成npy
-np.save(f"iData.npy", avgs)
-
-#⬇️和tData合并导出成txt 以方便在origin上操作
-tData = list(range(40,201,20))
-data = np.column_stack((avgs, tData))
-np.savetxt("datas.txt", data, delimiter="\t", header="X\tY", fmt="%.2f")
+    #⬇️和tData合并导出成txt 以方便在origin上操作
+    tData = list(range(40,201,20))
+    data = np.column_stack((avgs, tData))
+    np.savetxt("datas.txt", data, delimiter="\t", header="X\tY", fmt="%.2f")
